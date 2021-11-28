b8064def65c107e3aa59feeea34e404974c06337
mungue lee <cci454545@gmail.com>
2021-11-28 23:55:20 +0900

    Fix start time crash issue by FlipperAsyncStorage

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -3,8 +3,6 @@ import TestScreen from 'app/TestScreen';
 import {theme} from 'app/theme';
 import {DripsyProvider} from 'dripsy';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-// @ts-ignore: has not @types
-import FlipperAsyncStorage from 'rn-flipper-async-storage-advanced';
 
 const App = () => {
   return (
@@ -13,7 +11,6 @@ const App = () => {
         flex: 1,
       }}>
       <DripsyProvider theme={theme}>
-        {__DEV__ ? <FlipperAsyncStorage /> : null}
         <TestScreen />
       </DripsyProvider>
     </GestureHandlerRootView>
